a936ebb3587d1b1f7d3c3f3a3b1fb6224aa39fc7
abe.kentaro <abe.kentaro@zynas.co.jp>
2025-02-24 07:27:15 +0000

    サンプルコード作成

--- app/src/main/java/oop/sample/Money.java
+++ app/src/main/java/oop/sample/Money.java
@@ -14,8 +14,50 @@ import java.math.BigDecimal;
 // }
 
 // class Money {
+//     private int value; // 金額
+
+//     // 金額を加算する
+//     public Money add(Money other) {
+//         if (!this.currency.equals(other.currency)) {
+//             // 通貨が異なる場合はエラー
+//             throw new IllegalArgumentException("Currency mismatch.");
+//         }
+//         return new Money(this.amount + other.amount, this.currency);
+//     }
+
+//     public Money(int amount, String currency) {
+//         this.amount = amount;
+//         this.currency = currency;
+//     }
+// }
+
+class Money {
+    private int value; // 金額
+    private String currency; // 通貨
+
+    public void setValue(int newValue) {
+        this.value = newValue;
+    }
+    
+    public Money minus(int amount) {
+        return new Money(this.value - amount, this.currency);
+    }
+
+    public Money(int value, String currency) {
+        this.value = value;
+        this.currency = currency;
+    }
+}
+
+// class Money {
 // private int amount; // 金額
 // private String currency; // 通貨
+// private static final BigDecimal TAX_RATE = new BigDecimal("0.08"); // 税率
+
+// // 税額を計算する
+// public BigDecimal tax() {
+// return new BigDecimal(this.amount).multiply(TAX_RATE);
+// }
 
 // // 金額を加算する
 // public Money add(Money other) {
@@ -30,41 +72,16 @@ import java.math.BigDecimal;
 // this.amount = amount;
 // this.currency = currency;
 // }
+
+// boolean isDiscountable() {
+// return this.amount >= 1000;
 // }
 
-class Money {
-    private int amount; // 金額
-    private String currency; // 通貨
-    private static final BigDecimal TAX_RATE = new BigDecimal("0.08"); // 税率
+// Money multiply(int quantity) {
+// return new Money(this.amount * quantity, this.currency);
+// }
 
-    // 税額を計算する
-    public BigDecimal tax() {
-        return new BigDecimal(this.amount).multiply(TAX_RATE);
-    }
-
-    // 金額を加算する
-    public Money add(Money other) {
-        if (!this.currency.equals(other.currency)) {
-            // 通貨が異なる場合はエラー
-            throw new IllegalArgumentException("Currency mismatch.");
-        }
-        return new Money(this.amount + other.amount, this.currency);
-    }
-
-    public Money(int amount, String currency) {
-        this.amount = amount;
-        this.currency = currency;
-    }
-
-    boolean isDiscountable() {
-        return this.amount >= 1000;
-    }
-
-    Money multiply(int quantity) {
-        return new Money(this.amount * quantity, this.currency);
-    }
-
-    Money multiply(Quantity quantity) {
-        return new Money(this.amount * quantity.value, this.currency);
-    }
-}
+// Money multiply(Quantity quantity) {
+// return new Money(this.amount * quantity.value, this.currency);
+// }
+// }
--- app/src/main/java/oop/sample/UseMoney.java
+++ app/src/main/java/oop/sample/UseMoney.java
@@ -4,17 +4,44 @@ import java.math.BigDecimal;
 
 public class UseMoney {
     public void useMoney() {
-        Money money = new Money(1000, "JPY");
 
-        BigDecimal tax = money.tax();
+        // Money money = new Money(1000, "JPY");
+
+        // BigDecimal tax = money.tax();
 
         // int unitPrice = 100;
         // int quantity = 10;
         // int amount = amount(quantity, unitPrice);
 
-        Money unitPrice = new Money(100, "JPY");
-        Quantity quantity = new Quantity(10);
-        Money amount = amount(unitPrice, quantity);
+        // Money unitPrice = new Money(100, "JPY");
+        // Quantity quantity = new Quantity(10);
+        // Money amount = amount(unitPrice, quantity);
+    }
+    
+    void goodMoneyUsage() {
+        Money basePrice = new Money(3000, "JPY");
+        Money discounted = basePrice.minus(1000); // minus()メソッドは別のMoneyオブジェクトを作成して返す(後述)
+        Money option = new Money(1000, "JPY");    // 新しくMoneyオブジェクトを作る
+    }
+
+    void badMoneyUsage() {
+        // Money price = new Money(3000, "JPY");
+
+        // useMoney(price); // 金額を使用する
+
+        // price.setValue(2000);
+        // price = new Money(1000, "JPY");
+
+        // useMoney(price); // 金額を使用する
+
+        Money price = new Money(3000, "JPY");
+
+        price.setValue(2000);           // ✕ 値を書き換えている
+        price = new Money(1000, "JPY"); // ✕ 1つの変数に別の値を代入している
+    }
+
+    void useMoney(Money price) {
+
     }
 
     int discountCriteria = 10;
@@ -30,14 +57,14 @@ public class UseMoney {
         return unitPrice * quantity;
     }
 
-    Money discountAmount(Money unitPrice, Quantity quantity) {
-        return unitPrice.multiply(quantity);
-    }
+    // Money discountAmount(Money unitPrice, Quantity quantity) {
+    // return unitPrice.multiply(quantity);
+    // }
 
-    Money amount(Money unitPrice, Quantity quantity) {
-        if (quantity.isDiscountable()) {
-            return discountAmount(unitPrice, quantity);
-        }
-        return unitPrice.multiply(quantity);
-    }
+    // Money amount(Money unitPrice, Quantity quantity) {
+    // if (quantity.isDiscountable()) {
+    // return discountAmount(unitPrice, quantity);
+    // }
+    // return unitPrice.multiply(quantity);
+    // }
 }
